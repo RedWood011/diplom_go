830472932a4d8951f147dacf0be081fa274bdefa
Evgen Yaroshenko <yaev96@list.ru>
2023-08-16 13:12:07 +0300

    fix lint

--- server/internal/logger/logger.go
+++ server/internal/logger/logger.go
@@ -7,9 +7,5 @@ import (
 )
 
 func InitLogger() *slog.Logger {
-	var logger *slog.Logger
-
-	logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
-
-	return logger
+	return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 }
